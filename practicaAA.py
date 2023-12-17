bdfe38245a478ee9a80b9a492f6fb5c8a975c3b8
santtfg <98589920+santtfg@users.noreply.github.com>
2023-12-17 16:50:16 +0100

    Version actualizada

--- practicaAA.py
+++ practicaAA.py
@@ -1,10 +1,12 @@
+import os
 import  cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras import datasets, layers, models
 
-NUMBER_OF_CLASSES=4
+NUMBER_OF_CLASSES=2 # Skeleton y zombie
 PORCENTAJE_TEST=0.2
+IMAGE_SIZE = 50
 
 salir=False
 training_images=[]
@@ -28,30 +30,59 @@ while not salir:
             counter_arch+=1
 
             nueva_route=dire+"/"+archivo
-            image = cv2.imread(nueva_route)
+            image = cv.imread(nueva_route)
             if image is None:
                 print("No se pudo cargar la imagen en ",route)
 
             elif( counter_arch/num_archivos <= 1-PORCENTAJE_TEST): #esto es para dividirlo en datos de entrenamiento y datos de test
+                """
+                image_flatten=[]
+                for y in range(IMAGE_SIZE):
+                    for x in range(IMAGE_SIZE):
+                        valor_pixel = image[y,x]
+                        image_flatten.append(valor_pixel[0])
+                        image_flatten.append(valor_pixel[1])
+                        image_flatten.append(valor_pixel[2])
+                """
                 training_images.append(image)
                 training_labels.append(label)
-
             else:
-                testing_images.append(image)
-                testing_labels.append(label)
-
+                """
+                image_flatten=[]
+                for y in range(IMAGE_SIZE):
+                    for x in range(IMAGE_SIZE):
+                        valor_pixel = image[y,x]
+                        image_flatten.append(valor_pixel[0])
+                        image_flatten.append(valor_pixel[1])
+                        image_flatten.append(valor_pixel[2])
+                """
+                training_images.append(image)
+                training_labels.append(label)     
     else:
         salir=True
 
+"""
+for i in range(len(training_images)):
+    training_images[i] = [x / 255 for x in training_images[i]]
 
+for i in range(len(testing_images)):
+    testing_images[i] = [x / 255 for x in testing_images[i]]
+"""
+
+
+"""
 training_images = np.array(training_images)
 training_labels= np.array(training_labels)
-print(train_images_np.shape)
-print(train_labels)
+"""
 
+#print(np.shape(training_images))
+print(len(training_labels))
+print(len(training_images))
+
+"""
 testing_images = np.array(testing_images)
 testing_labels= np.array(testing_labels)
-
+"""
 
 model = models.Sequential()
 model.add(layers.Conv2D(50, (3,3), activation='relu', input_shape=(50,50,3)))
@@ -64,7 +95,7 @@ model.add(layers.Dense(NUMBER_OF_CLASSES, activation='softmax'))
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(training_images, training_labels, epoch=10, validation_data=(testing_images,testing_labels))
+model.fit(training_images, training_labels, epochs=10, validation_data=(testing_images,testing_labels))
 
 loss, accuracy = model.evaluate(testing_images, testing_labels)
 print("loss: ",loss)
